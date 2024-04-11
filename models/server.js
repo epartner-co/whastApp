21423f5d6ebf83af73bdf167469254cf6e4e555a
Sebastian <sebasguz.g12@gmail.com>
2024-04-11 18:22:55 -0500

    1.0.0

--- models/server.js
+++ models/server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
 
 class Server {
@@ -6,7 +9,10 @@ class Server {
     constructor() {
         this.app = express();
         this.host = process.env.HOST || '172.26.12.51'
-        this.port = process.env.PORT || 80;
+        this.port = process.env.HTTP_PORT || 80;
+        this.portHTTPS = process.env.HTTPS_PORT || 443;
+        this.keyPath = process.env.KEY_PATH || '/etc/letsencrypt/live/api.epartner-la.com/privkey.pem';
+        this.certPath = process.env.CERT_PATH || '/etc/letsencrypt/live/api.epartner-la.com/fullchain.pem';
         this.userPath = '/';
 
         // Rutas del servicio
@@ -21,10 +27,19 @@ class Server {
 
 
     listen() {
+        const httpsServerOptions = {
+            key: fs.readFileSync(this.keyPath),
+            cert: fs.readFileSync(this.certPath),
+        };
+        
         // Ejecutar servidor
-        this.app.listen(this.port,this.host, ()=>{
-            console.log(`Servidor corriendo en puerto: ${this.port} y en la ip: ${this.host}`);
-        });
+        const serverHttp = http.createServer(this.app);
+        serverHttp.listen(this.port, this.host);
+
+        // Servidor HTTPS
+        const serverHttps = https.createServer(httpsServerOptions, this.app);
+        serverHttps.listen(this.portHTTPS, this.host);
+
     }
 }
 
