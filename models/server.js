c710f3f0f382d3ce41b525bed858b55b82644285
Sebastian <sebasguz.g12@gmail.com>
2024-04-11 16:41:58 -0500

    1.0.0

--- models/server.js
+++ models/server.js
@@ -1,5 +1,10 @@
 const express = require('express');
-
+const https = require('node:https');
+const fs = require('node:fs');
+const options = {
+    key: fs.readFileSync('/etc/letsencrypt/live/api.epartner-la.com/fullchain.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/api.epartner-la.com/privkey.pem')
+};
 
 class Server {
 
@@ -20,11 +25,13 @@ class Server {
 
 
 
+
     listen() {
         // Ejecutar servidor
-        this.app.listen(this.port,this.host, ()=>{
-            console.log(`Servidor corriendo en puerto: ${this.port} y en la ip: ${this.host}`);
-        });
+        https.createServer(options, (req, res) => {
+            res.writeHead(200);
+            res.end('hello world\n');
+          }).listen(443);
     }
 }
 
