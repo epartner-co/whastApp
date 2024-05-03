1f8116ae64cf6263e2ba18552d1bcc9ead6a2308
Caminel1092 <79941008+Caminel1092@users.noreply.github.com>
2024-05-03 14:28:20 -0500

    test

--- routes/user.js
+++ routes/user.js
@@ -4,7 +4,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req,resp)=>{
-    resp.send("Soy un whastApp que nose ha enviado ")
+    resp.send("Soy un whastApp que nose ha enviado 2")
 });
 
 module.exports = router; 
